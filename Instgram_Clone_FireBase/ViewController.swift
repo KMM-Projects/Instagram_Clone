6616b9c0fac83655df28045e6dc323ad532f0d28
Patrik Kemeny <patrikkemeny@Patriks-MacBook-Pro.local>
2018-07-01 13:06:51 +1000

    init 01.07

---
+++ Instgram_Clone_FireBase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instgram_Clone_FireBase
+//
+//  Created by Patrik Kemeny on 1/7/18.
+//  Copyright © 2018 Patrik Kemeny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
