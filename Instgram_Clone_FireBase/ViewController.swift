1759ad502d009d09ca734b59c42d55b2e5b65530
Patrik Kemeny <patrikkemeny@192-168-1-3.tpgi.com.au>
2018-07-07 18:38:41 +1000

    Upload User Profile Pictures

--- Instgram_Clone_FireBase/ViewController.swift
+++ Instgram_Clone_FireBase/ViewController.swift
@@ -9,16 +9,42 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     
     let plusPhotoButton : UIButton  = {
         let button =  UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
-        
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
+    
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true // enable to edit picture (orezat, zooming etc.) to cierne pozadie
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    //to knwo what image we picked up with image picker controller
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+       //to pick the edited Image and work with edited image
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+           
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal) //set selected image as PlusButtonImage
+        
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal) //set selected image as PlusButtonImage
+        
+        }
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width/2  //put the picedk pisture into circle
+        plusPhotoButton.layer.masksToBounds = true
+         dismiss(animated: true, completion: nil)
+    }
+    
     //emailtextField
     let emailTextField: UITextField = {
         let tf = UITextField()
@@ -96,8 +122,33 @@ class ViewController: UIViewController {
             }
             
             print("Successfully created user:", user?.uid ?? "")
+            // upload profileImage to Cloude Storage
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }//30% compresion
+            let filename = NSUUID().uuidString //bc we want each photo have an random name/ prevent from picture overwriting in storage
+            Storage.storage().reference().child("profile_images").child(filename).putData(uploadData, metadata: nil, completion: { (metadata, error) in
+                if let error = error {
+                    print("Failed to upload Image: ", error)
+                    return
+                }
+                
+                guard let profileImageUrl = metadata?.downloadURL()?.absoluteString else {return}
+                print("Succesfully uploaded profile image!", profileImageUrl)
+                            guard let uid = user?.uid else { return }
+                let dictionaryValues = ["username" : username, "profileImageUrl": profileImageUrl]
+                            let values = [uid : dictionaryValues]
+                            Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                                if let error = err {
+                                    print("Failed to save user information into database:", err)
+                                    return
+                                }
+                                print("Succesfully saved user info into database")
+                            })
+            })
             
-        })
+            
+
+      })
     }
     
     override func viewDidLoad() {
